ae617757b972c2e0853eb7ea1bd4435f04e55be6
Veekant <veekant@gmail.com>
2023-04-10 01:04:38 -0400

    started work on raycasting

--- renderGame.py
+++ renderGame.py
@@ -5,8 +5,15 @@ from cmu_graphics import *
 import math
 import player
 
-def render(player, map):
-    pass
+def render(app, player, map):
+    for x in range(0, app.width, 1):
+        adjX = (2 * x / app.width) - 1
+        rayX = player.dirX + adjX * player.planeX
+        rayY = player.dirY + adjX * player.planeY
+        dist = rayCast(rayX, rayY, player, map)
 
-def rayCast():
+def rayCast(rayDirX, rayDirY, player, map):
+    posX, posY = player.x, player.y
+    mapX, mapY = int(posX), int(posY)
+    
     pass
