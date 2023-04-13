417c3509f39d514a7786d501c2929c3c1f655bb8
Veekant <veekant@gmail.com>
2023-04-13 05:11:17 -0400

    fixed rendering bugs

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import minimap
 
 def onAppStart(app):
     app.width, app.height = 1920, 1080
-    app.stepsPerSecond = 60
+    app.stepsPerSecond = 30
     app.map = load.loadMap("map1")
-    app.player = player.player((1, 1), 0, 90)
+    app.player = player.player((1.5, 1.5), 0, 90)
     app.minimap = minimap.minimap(app.map, app.player, app.width-300, app.height-300, 300, 300)
 
 def redrawAll(app):
+    renderGame.render(app.width, app.height, app.player, app.map)
     app.minimap.drawMap()
 
 def onStep(app):
@@ -27,8 +28,8 @@ def onKeyPress(app, key):
     if key == 'escape': app.stop()
 
 def onKeyHold(app, keys):
-    if 'q' in keys: app.player.rotate(0.1)
-    elif 'e' in keys: app.player.rotate(-0.1)
+    if 'q' in keys: app.player.rotate(-0.1)
+    elif 'e' in keys: app.player.rotate(0.1)
     elif 'w' in keys: app.player.move(0, -1/15)
     elif 'a' in keys: app.player.move(-1/15, 0)
     elif 's' in keys: app.player.move(0, 1/15)
--- renderGame.py
+++ renderGame.py
@@ -8,21 +8,35 @@ import player
 # raycasting algorithms from https://lodev.org/cgtutor/raycasting.html
 
 def render(width, height, player, map):
-    for x in range(0, width, 1):
+    for x in range(0, width, 4):
         adjX = (2 * x / width) - 1
         rayDirX = player.dirX + adjX * player.planeX
         rayDirY = player.dirY + adjX * player.planeY
         dist, side = rayCast(rayDirX, rayDirY, player, map)
-        drawVertLine(dist, side, height)
+        drawVertLine(x, dist, side, height)
 
 def rayCast(rayDirX, rayDirY, player, map):
     posX, posY = player.x, player.y
     mapX, mapY = int(posX), int(posY)
+    deltaDistX = math.inf if rayDirX == 0 else abs(1 / rayDirX)
+    deltaDistY = math.inf if rayDirY == 0 else abs(1 / rayDirY)
+    '''
+    if rayDirX >= 0:
+        stepX = 1
+        totalDistX = (mapX + 1 - posX) * deltaDistX
+    else:
+        stepX = -1
+        totalDistX = (posX - mapX) * deltaDistX
+    if rayDirY >= 0:
+        stepY = 1
+        totalDistY = (mapY + 1 - posY) * deltaDistY
+    else:
+        stepY = -1
+        totalDistY = (posY - mapY) * deltaDistY
+    '''
     stepX, stepY = sign(rayDirX), sign(rayDirY)
-    deltaDistX = math.inf if rayDirX == 0 else (1 / rayDirX)
-    deltaDistY = math.inf if rayDirY == 0 else (1 / rayDirY)
-    totalDistX = stepX * (posX - mapX - (stepX-1)//2) * deltaDistX
-    totalDistY = stepY * (posY - mapY - (stepY-1)//2) * deltaDistY
+    totalDistX = stepX * (mapX - posX + (stepX+1)//2) * deltaDistX
+    totalDistY = stepY * (mapY - posY + (stepY+1)//2) * deltaDistY
     side = 0
     hit = False
     while (not hit):
@@ -37,9 +51,15 @@ def rayCast(rayDirX, rayDirY, player, map):
         
         if map[mapX][mapY] > 0:
             hit = True
-    distance = (totalDistX - deltaDistX) if side == 0 else (totalDistY - deltaDistY)
-    return distance, side
+    dist = (totalDistX - deltaDistX) if side == 0 else (totalDistY - deltaDistY)
+    return dist, side
 
 def sign(num):
     if num == 0: return 1
-    return num // abs(num)
+    return int(num // abs(num))
+
+def drawVertLine(x, dist, side, height):
+    lineHeight = int(height/dist)
+    top = max(-lineHeight/2 + height/2, 0)
+    bottom = min(lineHeight/2 + height/2, height)
+    drawLine(x, bottom, x, top, fill='blue', lineWidth=4)
