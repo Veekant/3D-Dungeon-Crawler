55ca1a41e3f82c6c635cb47959241286d1ac7e47
Veekant <veekant@gmail.com>
2023-04-14 14:19:06 -0400

    moved some functionality to input

--- input.py
+++ input.py
@@ -1,3 +1,12 @@
 '''
 might handle some input stuff here
 '''
+
+def rotate(player, direction):
+    player.rotate(direction)
+
+def move(player, map, dx, dy):
+    player.move(dx, dy)
+    mapX, mapY = int(player.x), int(player.y)
+    if map[mapX][mapY] > 0:
+        player.move(-dx, -dy)
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import load
 import player
 import minimap
 import math
+import input
 
 def onAppStart(app):
     app.width, app.height = 1920, 1080
     app.stepsPerSecond = 60
     app.map = load.loadMap("map1")
-    app.player = player.player((1.5, 1.5), math.pi, 90)
+    app.player = player.player((1.5, 1.5), math.pi, 65)
     app.minimap = minimap.minimap(app.map, app.player, app.width-300, app.height-300, 300, 300)
 
 def redrawAll(app):
@@ -30,12 +31,13 @@ def onKeyPress(app, key):
     if key == 'escape': app.stop()
 
 def onKeyHold(app, keys):
-    if 'q' in keys: app.player.rotate(0.1)
-    elif 'e' in keys: app.player.rotate(-0.1)
-    elif 'w' in keys: app.player.move(0, -1/15)
-    elif 'a' in keys: app.player.move(1/15, 0)
-    elif 's' in keys: app.player.move(0, 1/15)
-    elif 'd' in keys: app.player.move(-1/15, 0)
+    if 'q' in keys: input.rotate(app.player, 0.1)
+    elif 'e' in keys: input.rotate(app.player, -0.1)
+
+    elif 'w' in keys: input.move(app.player, app.map, 0, -1/15)
+    elif 'a' in keys: input.move(app.player, app.map, 1/15, 0)
+    elif 's' in keys: input.move(app.player, app.map, 0, 1/15)
+    elif 'd' in keys: input.move(app.player, app.map, -1/15, 0)
 
 
 def main():
