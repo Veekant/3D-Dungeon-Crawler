f7f4fcf5524fb167102c3fbff9a82b89b5630734
Veekant <veekant@gmail.com>
2023-04-15 21:18:49 -0400

    stuff probably

--- renderGame.py
+++ renderGame.py
@@ -5,27 +5,26 @@ from cmu_graphics import *
 import math
 
 # raycasting algorithms from https://lodev.org/cgtutor/raycasting.html
-resolution = 8
-wallColors = ['gray', 'darkGray']
+resolution = 10
+wallColors = ['slateGray', 'lightSlateGray']
 ceilingColor = 'dimGray'
 floorColor = rgb(186, 140, 99)
 
 def render(width, height, player, map):
     drawRect(0, 0, width, height//2, fill=ceilingColor)
     drawRect(0, height//2, width, height, fill=floorColor)
-    for x in range(0, width, resolution):
+    for x in range(0, width+1, resolution):
         adjX = (2 * x / width) - 1
         rayDirX = player.dirX + adjX * player.planeX
         rayDirY = player.dirY + adjX * player.planeY
-        dist, side = rayCast(rayDirX, rayDirY, player, map)
+        dist, side = rayCast(player.x, player.y, rayDirX, rayDirY, map)
         drawVertLine(x, dist, side, height)
 
-def rayCast(rayDirX, rayDirY, player, map):
-    posX, posY = player.x, player.y
+def rayCast(posX, posY, dirX, dirY, map):
     mapX, mapY = int(posX), int(posY)
-    deltaDistX = math.inf if rayDirX == 0 else abs(1 / rayDirX)
-    deltaDistY = math.inf if rayDirY == 0 else abs(1 / rayDirY)
-    stepX, stepY = sign(rayDirX), sign(rayDirY)
+    deltaDistX = math.inf if dirX == 0 else abs(1 / dirX)
+    deltaDistY = math.inf if dirY == 0 else abs(1 / dirY)
+    stepX, stepY = sign(dirX), sign(dirY)
     totalDistX = stepX * (mapX - posX + (stepX == 1)) * deltaDistX
     totalDistY = stepY * (mapY - posY + (stepY == 1)) * deltaDistY
     side = 0
@@ -49,7 +48,7 @@ def sign(num):
     else: return 1
 
 def drawVertLine(x, dist, side, height):
-    lineHeight = int(height/dist)
+    lineHeight = int(height/dist) if dist != 0 else height
     top = max(-lineHeight/2 + height/2, 0)
     bottom = min(lineHeight/2 + height/2, height)
     color = wallColors[side]
