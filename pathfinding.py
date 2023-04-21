ed8844f0739e74af23bbf2f4dee4898314ecfd2e
Veekant <veekant@gmail.com>
2023-04-21 11:50:46 -0400

    started pathfinding

--- pathfinding.py
+++ pathfinding.py
@@ -5,3 +5,20 @@ TO DO:
 Implement helper functions to get map and stuff
 Implement A Star Algorithm
 '''
+
+import settings
+import math
+
+def getMapGraph():
+    map = settings.map
+    rows, cols = len(map), len(map[0])
+    graph = set()
+    for row in range(rows):
+        for col in range(cols):
+            if map[row][col] == 0:
+                graph.add((row, col))
+    return graph
+
+def getNeighborNodes(row, col):
+    neighborNodes = set()
+    print("want cry now")
